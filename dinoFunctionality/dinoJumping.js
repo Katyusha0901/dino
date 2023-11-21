f178a58ab3192a508b15dc0f2c7004df8e4f7778
Ekaterina Bondareva <1978231@bk.ru>
2023-11-22 02:07:41 +0600

    sdf

--- dinoFunctionality/dinoJumping.js
+++ dinoFunctionality/dinoJumping.js
@@ -2,10 +2,10 @@ import { dataStorage } from "../dataStorage.js";
 
 document.addEventListener("keydown", function (event) {
   if (event.code !== "ArrowUp") return;
-  let start = Date.now();
+  let startTime = Date.now();
   dinoJumping();
   function dinoJumping() {
-    let t = Date.now() - start;
+    let t = Date.now() - startTime;
     dataStorage.dino.y = (-1 / 3125) * (t * t) + (8 / 25) * t;
     if (t < 1000) {
       window.requestAnimationFrame(dinoJumping);
