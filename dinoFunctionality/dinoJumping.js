790bc756a76228894f7de4e826bb35a68a179bb9
Ekaterina Bondareva <1978231@bk.ru>
2023-11-22 12:20:05 +0600

    beautiful

--- dinoFunctionality/dinoJumping.js
+++ dinoFunctionality/dinoJumping.js
@@ -3,11 +3,16 @@ import { dataStorage } from "../dataStorage.js";
 document.addEventListener("keydown", function (event) {
   if (event.code !== "ArrowUp") return;
   if (dataStorage.dino.y !== 0) return;
+
   let startTime = Date.now();
+
   dinoJumping();
+
   function dinoJumping() {
     let t = Date.now() - startTime;
+
     dataStorage.dino.y = (-1 / 3125) * (t * t) + (8 / 25) * t;
+    
     if (dataStorage.dino.y >= 0) {
       window.requestAnimationFrame(dinoJumping);
     } else {
---
+++ dinoFunctionality/dinoRunning.js
@@ -0,0 +1,11 @@
+import { dataStorage } from "../dataStorage.js";
+let startTime = Date.now();
+dinoRunning();
+function dinoRunning() {
+  let t = Date.now() - startTime;
+  dataStorage.dino.x = 0.6*t;
+  if(dataStorage.gameStatus !== 'end'){
+    window.requestAnimationFrame(dinoRunning);
+
+  }
+}
