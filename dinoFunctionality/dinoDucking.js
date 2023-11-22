0168402a3a2e218e1a7b5fa558986ae323296dc6
Ekaterina Bondareva <1978231@bk.ru>
2023-11-22 12:03:48 +0600

    dinoDuncking

--- dinoFunctionality/dinoDucking.js
+++ dinoFunctionality/dinoDucking.js
@@ -1,5 +1,9 @@
 import { dataStorage } from "../dataStorage.js";
 
 document.addEventListener("keydown", function (event) {
-    
+  if (event.code === "ArrowDown") dataStorage.dino.isDucking = true;
+});
+
+document.addEventListener("keyup", function (event) {
+  if (event.code === "ArrowDown") dataStorage.dino.isDucking = false;
 });
