f861f61486ce5793988a01918d977b40d4bec659
Ekaterina Bondareva <1978231@bk.ru>
2023-11-27 16:58:41 +0600

    correct

--- dinoFunctionality/dinoDucking.js
+++ dinoFunctionality/dinoDucking.js
@@ -4,11 +4,15 @@ dinoDucking();
 
 function dinoDucking() {
   document.addEventListener("keydown", function (event) {
-    if (event.code === "ArrowDown") dataStorage.dino.isDucking = true;
+    if (event.code === "ArrowDown") {
+      dataStorage.dino.isDucking = true;
+      dataStorage.dino.y = 0;
+    }
   });
 
   document.addEventListener("keyup", function (event) {
     if (event.code === "ArrowDown") dataStorage.dino.isDucking = false;
   });
+
   window.requestAnimationFrame(dinoDucking);
 }
